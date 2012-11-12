38bcbc281883d8779105460fdd833c13071027fe
Federico <federico.aloi@gmail.com>
2012-11-12 17:34:37 -0300

    Agregado ControlBindingHelpers

---
+++ Aplicacion Desktop/GrouponDesktop/Helpers/ControlBindingHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GrouponDesktop.Helpers
+{
+    public static class ControlBindingHelpers
+    {
+        public static void BindTextTo(this Control control, object model, string propertyName)
+        {
+            control.DataBindings.Add("Text", model, propertyName);
+        }
+
+        public static void BindTextToDate(this Control control, object model, string propertyName, string format)
+        {
+            var binding = new Binding("Text", model, propertyName);
+            binding.Format += (sender, args) => args.Value = ((DateTime)args.Value).ToString(format);
+
+            control.DataBindings.Add(binding);
+        }
+    }
+}
+
