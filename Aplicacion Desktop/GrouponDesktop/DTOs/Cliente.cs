ecba7bc006bc40c89b44abb21365fc32fce12666
Tomas Fontenla <tfontenla@hotmail.com>
2012-11-12 15:07:51 -0300

    Agregado Rol y Cliente cs

---
+++ Aplicacion Desktop/GrouponDesktop/DTOs/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GrouponDesktop.DTOs
+{
+    class Cliente
+    {
+        public long id { set; get; }
+        public string nombre { set; get; }
+        public string apellido { set; get; }
+        public int dni { set; get; }
+        public string mail { set; get; }
+        public int telefono { set; get; }
+        public string direccion { set; get; }
+        public int cod_postal { set; get; }
+        public datetime fecha_nac { set; get; }
+        public int saldo_actual { set; get; }
+    }
+}
---
+++ Aplicacion Desktop/GrouponDesktop/DTOs/Rol.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GrouponDesktop.DTOs
+{
+    class Rol
+    {
+        public long id { set; get; }
+        public string descripcion { set; get; }
+        public int estado { set; get; }
+
+    }
+}
