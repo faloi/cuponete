1cc8201350925a92b56851ad8366e1bf75133656
Federico <federico.aloi@gmail.com>
2012-11-12 17:35:02 -0300

    Agregada vista LoginView y modificado Program para que la abra al principio

---
+++ Aplicacion Desktop/GrouponDesktop/ManagementUsuario/LoginView.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace GrouponDesktop.ManagementUsuario
+{
+    partial class LoginView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(97, 17);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(158, 20);
+            this.usernameTextBox.TabIndex = 2;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(97, 44);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(158, 20);
+            this.passwordTextBox.TabIndex = 3;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(32, 94);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(223, 38);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "Iniciar sesion";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // LoginView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 152);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginView";
+            this.Text = "LoginView";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button loginButton;
+    }
+}
---
+++ Aplicacion Desktop/GrouponDesktop/ManagementUsuario/LoginView.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Forms;
+using GrouponDesktop.DTOs;
+using GrouponDesktop.Helpers;
+using GrouponDesktop.Repositories;
+
+namespace GrouponDesktop.ManagementUsuario
+{
+    public partial class LoginView : Form
+    {
+        private UserRepository repository;
+        private Usuario model;
+
+        public LoginView()
+        {
+            InitializeComponent();
+            
+            this.repository = RepoFactory.User;
+            this.model = new Usuario();
+
+            this.CreateBindings();
+        }
+
+        private void CreateBindings()
+        {
+            this.usernameTextBox.BindTextTo(this.model, "username");
+            this.passwordTextBox.BindTextTo(this.model, "password");
+
+            this.loginButton.Click +=
+                (sender, args) => this.repository.Login(this.model);
+        }
+    }
+}
