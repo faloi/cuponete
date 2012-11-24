0d4c6db7fc70ddfbc3c11065b88070d00c93e591
unknown <mzavatto9@gmail.com>
2012-11-24 03:44:53 -0600

    Agregue CiudadHome y RolHome

---
+++ src/GrouponDesktop/Homes/CiudadHome.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using GrouponDesktop.Sql;
+
+namespace GrouponDesktop.Homes
+{
+    public class CiudadHome : Home
+    {
+
+        public CiudadHome(string connectionString)
+            : base(connectionString)
+        {
+            
+        }
+
+
+        public DataTable CiudadesDisponibles()
+        {
+            try
+            {
+                return sqlRunner
+                    .Select("SELECT * FROM RANDOM.Ciudad");
+            }
+            catch (NoResultsException e)
+            {
+                throw new ApplicationException("No hay Resultados", e);
+            }
+            
+        }
+
+/*
+        private static class Queries
+        {
+
+        }
+*/
+    }
+}
---
+++ src/GrouponDesktop/Homes/RolHome.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using GrouponDesktop.Sql;
+
+namespace GrouponDesktop.Homes
+{
+    public class RolHome : Home
+    {
+
+        public RolHome(string connectionString)
+            : base(connectionString)
+        {
+            
+        }
+
+
+        public DataTable RolesDisponibles()
+        {
+            try
+            {
+                return sqlRunner
+                    .Select("SELECT * FROM RANDOM.Rol WHERE descripcion != 'Administrador'");
+            }
+            catch (NoResultsException e)
+            {
+                throw new ApplicationException("El usuario no existe", e);
+            }
+            
+        }
+
+/*
+        private static class Queries
+        {
+
+        }
+*/
+    }
+}
