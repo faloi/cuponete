b2b63feabf76df57b57e0fc94854847ea39e406e
Tiziano Perez Stolfa <tperezstolfa@gmail.com>
2012-11-29 17:49:15 -0300

    termine los procedures para agregar usuarios
    ahora realizan los chequeos para que no se repitan las cosas y devuelven
    el correspondiente error con RAISERROR

--- data/RANDOM/stored_procedures.sql
+++ data/RANDOM/stored_procedures.sql
@@ -13,27 +13,93 @@ BEGIN
 END
 GO
 
-
-/*Estos dos capaz combiene hacerlos funciones para que validen que no existan datos repetidos y demas, otra opcion es validar eso desde c#, pero creo que sería mejor hacerlo aca*/
 --Registro de cliente
-create procedure RANDOM.RegistrarCliente @username nvchar(255), @password nvchar(255), @nombre nvchar(255), @apellidos nvchar(255), @dni numeric(18,0), @mail nvchar(100), @telefono numeric (18,0), @direccion nvchar(255), @cod_postal numeric(18,0), @fecha_nac datetime
+create procedure RANDOM.RegistrarCliente @username nvarchar(255), @password nvarchar(255), @nombre nvarchar(255), @apellidos nvarchar(255), @dni numeric(18,0), @mail nvarchar(100), @telefono numeric (18,0), @direccion nvarchar(255), @cod_postal numeric(18,0), @fecha_nac datetime
 as
-begin
+begin transaction
+	if exists (select username from RANDOM.Usuario where @username = username)
+	begin
+		rollback
+		raiserror('El nombre de usuario ya existe', 0, 0)
+	end	
+	else
+	begin
+		if exists (select dni from RANDOM.Cliente where dni = @dni)
+		begin
+			rollback
+			raiserror('El DNI ya esta registrado para otro cliente', 0, 0)
+		end	
+		else
+		begin
+			if exists (select telefono from RANDOM.Cliente where telefono = @telefono)
+			begin
+				rollback
+				raiserror('El telefono ya esta registrado para otro cliente', 0, 0)
+			end
+			else
+			begin
+				if exists (select mail from RANDOM.Cliente where mail = @mail)
+				begin
+				rollback
+				raiserror('El mail ya está registrado para otro cliente', 0, 0)
+				end
+			end
+		end	
+	end
+			
 	insert into RANDOM.Usuario(username, password, id_rol, dni_cuit, tipo, estado, fallas)
-	values(@username, @password, select id_rol from RANDOM.rol where descripcion = 'Cliente', @dni, 'Cliente', 1, 0)
+	values(@username, @password, (select id_rol from RANDOM.rol where descripcion = 'Cliente'), @dni, 'Cliente', 1, 0)
 	insert into RANDOM.Cliente(nombre, apellido, dni, mail, telefono, direccion, cod_postal, fecha_nac, saldo_actual)
 	values(@nombre, @apellidos, @dni, @mail, @telefono, @direccion, @cod_postal, @fecha_nac, 10)
-end
+commit
 go
 
 --Registro de un proveedor
-create procedure RANDOM.RegistrarProveedor @username nvchar(255), @password nvchar(255), @razon_social nvchar(255), @mail nvchar(100), @telefono numeric (18,0), @direccion nvchar(255), @cod_postal numeric(18,0), @id_ciudad bigint, @cuit nvchar(20), @id_rubro bigint, @contacto_nombre nvchar(100)
+create procedure RANDOM.RegistrarProveedor @username nvarchar(255), @password nvarchar(255), @razon_social nvarchar(255), @mail nvarchar(100), @telefono numeric (18,0), @direccion nvarchar(255), @cod_postal numeric(18,0), @id_ciudad bigint, @cuit nvarchar(20), @id_rubro bigint, @contacto_nombre nvarchar(100)
 as
-begin
+begin transaction
+	if exists (select username from RANDOM.Usuario where @username = username)
+	begin
+		rollback
+		raiserror('El nombre de usuario ya existe', 0, 0)
+	end	
+	else
+	begin
+		if exists (select razon_social from RANDOM.Proveedor where razon_social = @razon_social)
+		begin
+			rollback
+			raiserror('La razon social ya esta registrada para otro proveedor', 0, 0)
+		end	
+		else
+		begin
+			if exists (select cuit from RANDOM.Proveedor where cuit = @cuit)
+			begin
+				rollback
+				raiserror('El CUIT ya esta registrado para otro proveedor', 0, 0)
+			end
+			else
+			begin
+				if exists (select telefono from RANDOM.Proveedor where telefono = @telefono)
+				begin
+					rollback
+					raiserror('El telefono ya esta registrado para otro proveedor', 0, 0)
+				end
+				else
+				begin
+					if exists (select mail from RANDOM.Proveedor where mail = @mail)
+					begin
+						rollback
+						raiserror('El mail ya está registrado para otro proveedor', 0, 0)
+					end
+				end
+			end
+		end	
+	end
+
 	insert into RANDOM.Usuario(username, password, id_rol, dni_cuit, tipo, estado, fallas)
-	values(@username, @password, select id_rol from RANDOM.rol where descripcion = 'Proveedor', @dni, 'Proveedor', 1, 0)
+	values(@username, @password, (select id_rol from RANDOM.rol where descripcion = 'Proveedor'), @cuit, 'Proveedor', 1, 0)
 	insert into RANDOM.Proveedor(razon_social, mail, telefono, direccion, cod_postal, id_ciudad, cuit, id_rubro, contacto_nombre)
 	values(@razon_social, @mail, @telefono, @direccion, @cod_postal, @id_ciudad, @cuit, @id_rubro, @contacto_nombre)
-end
+commit
 go
-/**********************************************************************************************************************************************************************************/
+
