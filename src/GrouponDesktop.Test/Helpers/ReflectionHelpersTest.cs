d02e786752f6a16de0878a6bdfa33da6395e29c5
Federico <federico.aloi@gmail.com>
2012-12-07 01:12:48 -0300

    Implementado ReflectionHelpers.SetPropertyIfExists

---
+++ src/GrouponDesktop.Test/Helpers/ReflectionHelpersTest.cs
@@ -0,0 +1,28 @@
+﻿using GrouponDesktop.DTOs;
+using GrouponDesktop.Helpers;
+using Xunit;
+
+namespace GrouponDesktop.Test.Helpers
+{
+    public class ReflectionHelpersTest
+    {
+        [Fact]
+        public void Set_property_should_change_the_value_if_the_property_exists()
+        {
+            var cliente = new Cliente {apellido = "Aloi", cod_postal = 1406};
+            
+            cliente.SetPropertyIfExists("apellido", "Gonzalez");
+            cliente.SetPropertyIfExists("cod_postal", 1125);
+
+            Assert.Equal("Gonzalez", cliente.apellido);
+            Assert.Equal(1125, cliente.cod_postal);
+        }
+
+        [Fact]
+        public void Set_property_shouldnt_do_anything_if_it_doesnt_exist()
+        {
+            var cliente = new Cliente();
+            Assert.DoesNotThrow(() => cliente.SetPropertyIfExists("sarasa", 15));
+        }
+    }
+}
--- src/GrouponDesktop/Helpers/ReflectionHelpers.cs
+++ src/GrouponDesktop/Helpers/ReflectionHelpers.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -8,6 +9,15 @@ namespace GrouponDesktop.Helpers
         public static bool IsAutomaticProperty(this PropertyInfo property)
         {
             return (property.GetGetMethod() ?? property.GetSetMethod()).IsDefined(typeof (CompilerGeneratedAttribute), false);
+        }
+
+        public static void SetPropertyIfExists(this object instance, string name, object value)
+        {
+            try
+            {
+                instance.GetType().GetProperty(name).SetValue(instance, value, null);
+            }
+            catch (NullReferenceException) {}
         }
     }
 }
