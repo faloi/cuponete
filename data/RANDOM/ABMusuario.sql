a84cd5f07425d28344bd77fa3a3cc7f0796d24f8
Tiziano Perez Stolfa <tperezstolfa@gmail.com>
2012-12-10 22:32:48 -0300

    modifique cambiar rol
    ahora tiene un parametro que indica si hay que llamar o no a registrar
    los datos

--- data/RANDOM/ABMusuario.sql
+++ data/RANDOM/ABMusuario.sql
@@ -39,10 +39,12 @@ end
 go
 
 
---Cambiar rol: RANDOM.CambiarRol (parametros necesarios. Es importante, despues del cambio a rol de proveedor o cliente, llamar al procedure registrar de dicho rol)
-create procedure RANDOM.CambiarRol @id_usuario bigint out, @id_rol bigint out
+--Cambiar rol: RANDOM.CambiarRol (parametros necesarios. Es importante, despues del cambio a rol de proveedor o cliente, llamar al procedure registrar de dicho rol si la variable @llamar_registro está en 1)
+create procedure RANDOM.CambiarRol @id_usuario bigint out, @id_rol bigint out, @llamar_registro numeric(1,0) out
 as
 begin transaction
+	set @llamar_registro = 0
+	
 	if (select id_rol from RANDOM.Usuario where @id_usuario = id_usuario) = @id_rol
 	begin
 		rollback
@@ -50,15 +52,35 @@ begin transaction
 		return
 	end	
 	
-	if ((select id_rol from RANDOM.Usuario where id_usuario = @id_usuario) = 2 /*2 es cliente*/)
+	if ((select id_rol from RANDOM.Usuario where id_usuario = @id_usuario) = NULL)
 	begin
-		delete from RANDOM.Cliente_x_Ciudad where id_cliente = @id_usuario
-		delete from RANDOM.Cliente where id_usuario = @id_usuario
+		if ((exists (select 1 from RANDOM.Usuario u inner join RANDOM.Cliente c on u.id_usuario = c.id_usuario)) and @id_rol != 2 /*2 es cliente*/)
+		begin
+			delete from RANDOM.Cliente_x_Ciudad where id_cliente = @id_usuario
+			delete from RANDOM.Cliente where id_usuario = @id_usuario
+			set @llamar_registro = 1
+		end
+		
+		if ((exists (select 1 from RANDOM.Usuario u inner join RANDOM.Proveedor p on u.id_usuario = p.id_usuario)) and @id_rol != 3 /*3 es proveedor*/)
+		begin
+			delete from RANDOM.Proveedor where id_usuario = @id_usuario
+			set @llamar_registro = 1
+		end
 	end
-	
-	if ((select id_rol from RANDOM.Usuario where id_usuario = @id_usuario) = 3 /*3 es proveedor*/)
+	else
 	begin
-		delete from RANDOM.Proveedor where id_usuario = @id_usuario
+		if ((select id_rol from RANDOM.Usuario where id_usuario = @id_usuario) = 2 /*2 es cliente*/)
+		begin
+			delete from RANDOM.Cliente_x_Ciudad where id_cliente = @id_usuario
+			delete from RANDOM.Cliente where id_usuario = @id_usuario
+			set @llamar_registro = 1
+		end
+		
+		if ((select id_rol from RANDOM.Usuario where id_usuario = @id_usuario) = 3 /*3 es proveedor*/)
+		begin
+			delete from RANDOM.Proveedor where id_usuario = @id_usuario
+			set @llamar_registro = 1
+		end
 	end
 	
 	update RANDOM.Usuario
--- data/RANDOM/triggers.sql
+++ data/RANDOM/triggers.sql
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /** Cambia el rol de los clientes a NULL cuando se deshabilita dicho rol en la tabla rol **/
 create trigger RANDOM.inhabilitar_rol ON RANDOM.Rol
 after update
@@ -6,48 +6,10 @@ as
 begin
 	if exists (select 1 from inserted where estado = 0)
 	begin
-	    
-	    if (select estado from RANDOM.Rol where id_rol = 2 /*2 es cliente*/) = 0
-		begin
-			delete from RANDOM.Cliente_x_Ciudad
-			delete from RANDOM.Cliente
-		end
-	
-		if (select estado from RANDOM.Rol where id_rol = 3 /*3 es proveedor*/) = 0 
-		begin
-			delete from RANDOM.Proveedor
-		end
-	      
 		update RANDOM.Usuario
-		set id_rol = NULL
+		set id_rol = NULL	
 		where id_rol = (select id_rol from inserted)
-	end           
+	end
 end
 go
-=======
-/** Cambia el rol de los clientes a NULL cuando se deshabilita dicho rol en la tabla rol **/
-create trigger RANDOM.inhabilitar_rol ON RANDOM.Rol
-after update
-as
-begin
-	if exists (select 1 from inserted where estado = 0)
-	begin
-	    
-	    if (select estado from RANDOM.Rol where id_rol = 2 /*2 es cliente*/) = 0
-		begin
-			delete from RANDOM.Cliente_x_Ciudad
-			delete from RANDOM.Cliente
-		end
-	
-		if (select estado from RANDOM.Rol where id_rol = 3 /*3 es proveedor*/) = 0 
-		begin
-			delete from RANDOM.Proveedor
-		end
-	      
-		update RANDOM.Usuario
-		set id_rol = NULL
-		where id_rol = (select id_rol from inserted)
-	end           
-end
-go
->>>>>>> agregada vista para historial de cupones
+
