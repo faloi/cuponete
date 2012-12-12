a3f449e6759e884c76db7086d76da40eebfcfa7b
unknown <mzavatto9@gmail.com>
2012-12-12 00:32:49 -0300

    Arreglo CargarCredito

--- src/GrouponDesktop/Homes/UsuarioHome.cs
+++ src/GrouponDesktop/Homes/UsuarioHome.cs
@@ -171,8 +171,9 @@ namespace GrouponDesktop.Homes
                                                               "carga_credito", "fecha", "id_forma_pago", "nro_tarjeta",
                                                               "cod_seguridad_tarjeta", "fecha_vto_tarjeta")
 
+
                                  };
-            this.RunProcedures(procedures);
+            this.Run(procedures);
         }
 
         public void ModificarCliente(Cliente cliente, IEnumerable<Ciudad> ciudadesAgregadas, IEnumerable<Ciudad> ciudadesEliminadas)
