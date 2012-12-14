6a20e5733510eaf9343f9f4daa63918f695c604d
Federico <federico.aloi@gmail.com>
2012-12-14 20:44:55 -0300

    El listado estadistico ahora trae por default el año y el semestre del archivo config

--- src/GrouponDesktop/DTOs/Estadistica.cs
+++ src/GrouponDesktop/DTOs/Estadistica.cs
@@ -1,3 +1,5 @@
+using GrouponDesktop.Helpers;
+
 namespace GrouponDesktop.DTOs
 {
     public class Estadistica
@@ -5,6 +7,14 @@ namespace GrouponDesktop.DTOs
         public TipoListado TipoListado { get; set; }
         public int Semestre { get; set; }
         public int Anio { get; set; }
+
+        public Estadistica()
+        {
+            var fechaSistema = ControlBindingHelpers.FechaSistema;
+
+            this.Semestre = fechaSistema.GetSemester();
+            this.Anio = fechaSistema.Year;
+        }
     }
 
     public enum TipoListado
