3357ecf4da0600c52fb6880496ef329039584b09
unknown <mzavatto9@gmail.com>
2012-12-14 05:16:26 -0300

    Para DevolverCupon

---
+++ src/GrouponDesktop/DTOs/CuponParaDevolucion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GrouponDesktop.Helpers;
+
+namespace GrouponDesktop.DTOs
+{
+    public class CuponParaDevolucion
+    {
+        public DateTime fecha_devolucion { set; get; }
+        public long id_cliente { set; get; }
+        public long id_cupon { set; get; }
+        public long id_compra { set; get; }
+        public long codigo_compra { set; get; }
+
+    }
+}
