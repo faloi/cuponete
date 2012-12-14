17ce8270ec5f6976bbbb4506012f07754cf4ebfb
Federico <federico.aloi@gmail.com>
2012-12-14 20:44:16 -0300

    Implementado DateTimeHelpers.GetSemester()

---
+++ src/GrouponDesktop/Helpers/DateTimeHelpers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GrouponDesktop.Helpers
+{
+    public static class DateTimeHelpers
+    {
+        public static int GetSemester(this DateTime dateTime)
+        {
+            return (dateTime.Month - 1) / 6 + 1;
+        }
+    }
+}
