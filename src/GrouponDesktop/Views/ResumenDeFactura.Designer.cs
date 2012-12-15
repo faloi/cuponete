12fff232e2ce1ccb10ab034e16e6202b7112509b
Federico <federico.aloi@gmail.com>
2012-12-15 04:55:33 -0300

    Implementada vista ResumenDeFactura

--- src/GrouponDesktop/Views/ResumenDeFactura.Designer.cs
+++ src/GrouponDesktop/Views/ResumenDeFactura.Designer.cs
@@ -28,18 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.labelProveedor = new System.Windows.Forms.Label();
-            this.textBoxProveedor = new System.Windows.Forms.TextBox();
+            this.numeroFactura = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.CodigoCupon = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Monto = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.montoTotal = new System.Windows.Forms.TextBox();
             this.buttonAceptar = new System.Windows.Forms.Button();
-            this.buttonCancelar = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
             this.SuspendLayout();
             // 
             // labelProveedor
@@ -52,119 +48,71 @@
             this.labelProveedor.TabIndex = 30;
             this.labelProveedor.Text = "Resumen De Factura";
             // 
-            // textBoxProveedor
+            // numeroFactura
             // 
-            this.textBoxProveedor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBoxProveedor.Location = new System.Drawing.Point(156, 64);
-            this.textBoxProveedor.Name = "textBoxProveedor";
-            this.textBoxProveedor.ReadOnly = true;
-            this.textBoxProveedor.Size = new System.Drawing.Size(169, 20);
-            this.textBoxProveedor.TabIndex = 0;
+            this.numeroFactura.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.numeroFactura.Location = new System.Drawing.Point(168, 78);
+            this.numeroFactura.Name = "numeroFactura";
+            this.numeroFactura.ReadOnly = true;
+            this.numeroFactura.Size = new System.Drawing.Size(157, 20);
+            this.numeroFactura.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(70, 64);
+            this.label1.Location = new System.Drawing.Point(70, 77);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(80, 21);
             this.label1.TabIndex = 32;
             this.label1.Text = "N° Factura";
             // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.CodigoCupon,
-            this.Monto});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 103);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(367, 134);
-            this.dataGridView1.TabIndex = 1;
-            // 
-            // CodigoCupon
-            // 
-            this.CodigoCupon.HeaderText = "Código Cupón";
-            this.CodigoCupon.Name = "CodigoCupon";
-            this.CodigoCupon.ReadOnly = true;
-            // 
-            // Monto
-            // 
-            this.Monto.HeaderText = "Monto";
-            this.Monto.Name = "Monto";
-            this.Monto.ReadOnly = true;
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(70, 249);
+            this.label2.Location = new System.Drawing.Point(70, 105);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(92, 21);
             this.label2.TabIndex = 38;
             this.label2.Text = "Monto Total";
             // 
-            // textBox1
+            // montoTotal
             // 
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Location = new System.Drawing.Point(168, 249);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(157, 20);
-            this.textBox1.TabIndex = 2;
+            this.montoTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.montoTotal.Location = new System.Drawing.Point(168, 105);
+            this.montoTotal.Name = "montoTotal";
+            this.montoTotal.ReadOnly = true;
+            this.montoTotal.Size = new System.Drawing.Size(157, 20);
+            this.montoTotal.TabIndex = 2;
             // 
             // buttonAceptar
             // 
             this.buttonAceptar.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonAceptar.Location = new System.Drawing.Point(22, 293);
+            this.buttonAceptar.Location = new System.Drawing.Point(129, 166);
             this.buttonAceptar.Name = "buttonAceptar";
             this.buttonAceptar.Size = new System.Drawing.Size(140, 29);
             this.buttonAceptar.TabIndex = 3;
             this.buttonAceptar.Text = "Aceptar";
             this.buttonAceptar.UseVisualStyleBackColor = true;
             // 
-            // buttonCancelar
-            // 
-            this.buttonCancelar.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonCancelar.Location = new System.Drawing.Point(239, 293);
-            this.buttonCancelar.Name = "buttonCancelar";
-            this.buttonCancelar.Size = new System.Drawing.Size(140, 29);
-            this.buttonCancelar.TabIndex = 4;
-            this.buttonCancelar.Text = "Cancelar";
-            this.buttonCancelar.UseVisualStyleBackColor = true;
-            this.buttonCancelar.Click += new System.EventHandler(this.buttonCancelar_Click);
-            // 
             // ResumenDeFactura
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(393, 334);
-            this.Controls.Add(this.buttonCancelar);
+            this.ClientSize = new System.Drawing.Size(393, 217);
             this.Controls.Add(this.buttonAceptar);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.montoTotal);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.labelProveedor);
-            this.Controls.Add(this.textBoxProveedor);
+            this.Controls.Add(this.numeroFactura);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.Name = "ResumenDeFactura";
             this.Text = "ResumenDeFactura";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -173,14 +121,10 @@
         #endregion
 
         private System.Windows.Forms.Label labelProveedor;
-        private System.Windows.Forms.TextBox textBoxProveedor;
+        private System.Windows.Forms.TextBox numeroFactura;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn CodigoCupon;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Monto;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox montoTotal;
         private System.Windows.Forms.Button buttonAceptar;
-        private System.Windows.Forms.Button buttonCancelar;
     }
 }
--- src/GrouponDesktop/Views/ResumenDeFactura.cs
+++ src/GrouponDesktop/Views/ResumenDeFactura.cs
@@ -6,17 +6,28 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using GrouponDesktop.DTOs;
+using GrouponDesktop.Helpers;
 
 namespace GrouponDesktop.Views
 {
-    public partial class ResumenDeFactura : Form
+    public partial class ResumenDeFactura : DefaultView
     {
-        public ResumenDeFactura()
+        public ResumenDeFactura(Factura factura)
         {
-            InitializeComponent();
+            this.InitializeComponent();
+            this.CreateBindings(this.buttonAceptar);
+
+            this.SetBindingSource(factura);
         }
 
-        private void buttonCancelar_Click(object sender, EventArgs e)
+        protected override void CreateSpecificBindings()
+        {
+            this.numeroFactura.BindTextTo(this.model, "nro_factura");
+            this.montoTotal.BindTextTo(this.model, "monto_total");
+        }
+
+        protected override void ExecSubmit()
         {
             this.Close();
         }
