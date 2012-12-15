bf04f93102178d1216ccc5716a4794dd1c2fe3da
Tiziano Perez Stolfa <tperezstolfa@gmail.com>
2012-12-14 22:00:58 -0300

    arreglada la estadistica (espero)

--- src/GrouponDesktop/Homes/EstadisticasHome.cs
+++ src/GrouponDesktop/Homes/EstadisticasHome.cs
@@ -12,13 +12,19 @@ namespace GrouponDesktop.Homes
         {
             const string QUERY =
                 @"select distinct top 5 razon_social, telefono, porcentaje_devolucion = 
-			                (select count(*) from RANDOM.Cupon_Devuelto cdev1 
+                    case when ((select count(*) from RANDOM.Cupon_Comprado ccom1 
+							 inner join RANDOM.Cupon c1 on ccom1.id_cupon = c1.id_cupon 
+							 where c1.id_proveedor = c.id_proveedor and year(ccom1.fecha_compra) = {0} and floor(((month(ccom1.fecha_compra)) - 1) / 6) + 1 = {1}) = 0)
+                    then 0
+                    else
+			                isnull((select count(*) from RANDOM.Cupon_Devuelto cdev1 
 							 inner join RANDOM.Cupon_Comprado ccom1 on cdev1.id_compra = ccom1.id_compra 
 							 inner join RANDOM.Cupon c1 on ccom1.id_cupon = c1.id_cupon
 							 where c1.id_proveedor = c.id_proveedor and year(cdev1.fecha_devolucion) = {0} and floor(((month(cdev1.fecha_devolucion)) - 1) / 6) + 1 = {1}) * 100 /
 			                (select count(*) from RANDOM.Cupon_Comprado ccom1 
 							 inner join RANDOM.Cupon c1 on ccom1.id_cupon = c1.id_cupon 
-							 where c1.id_proveedor = c.id_proveedor and year(ccom1.fecha_compra) = {0} and floor(((month(ccom1.fecha_compra)) - 1) / 6) + 1 = {1})
+							 where c1.id_proveedor = c.id_proveedor and year(ccom1.fecha_compra) = {0} and floor(((month(ccom1.fecha_compra)) - 1) / 6) + 1 = {1}), 0)
+                    end
                 from RANDOM.Cupon c
 	            inner join RANDOM.Proveedor p on p.id_usuario = c.id_proveedor
                 order by porcentaje_devolucion desc";
